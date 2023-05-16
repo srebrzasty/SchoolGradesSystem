13d43dfed2b836241b5191d4703e99301e18bf23
4xTime <88463120+4xTime@users.noreply.github.com>
2023-05-16 21:42:40 +0200

    refaktoryzowany kod, który wykorzystuje wektory i obsługuje błędy wejścia
        1.Dodanie wektora grades i weights_of_grades:
            Zamień deklarację tablic float grades[8] na std::vector<float> grades.
            Zamień deklarację tablic int weights_of_grades[8] na std::vector<int> weights_of_grades.
            Dodaj odpowiednie #include dla vector: #include <vector>.
    
        2.Obsługa błędów przy wprowadzaniu ocen i wag:
            Dodaj kod błędu GRADES_ERROR, który oznacza, że wprowadzona ocena jest większa niż 6.
            Dodaj kod błędu WEIGHTS_OF_GRADES_ERROR, który oznacza, że wprowadzona waga jest większa niż 6.
            Zdefiniuj funkcję edit_vector_in_case_of_error, która przyjmuje referencje do wektorów grades i weights_of_grades, pozycję, na której wystąpił błąd, oraz kod błędu.
            W odpowiednich miejscach w pętli dodawania ocen i wag, sprawdź czy wartości są większe niż 6 i wywołaj funkcję edit_vector_in_case_of_error w razie potrzeby.
    
        3.Poprawienie formatowania i wyświetlania komunikatów:
            Dodaj odpowiednie wcięcia w kodzie dla czytelności.
            Popraw błędy ortograficzne i interpunkcyjne w wyświetlanych komunikatach.

--- countyourgrade.cpp
+++ countyourgrade.cpp
@@ -1,7 +1,25 @@
 #include <iostream>
-
+#include <vector>
+#include <limits>
+//Definiuje kody błędu
+#define GRADES_ERROR 0
+#define WEIGHTS_OF_GRADES_ERROR 1
 using namespace std;
  
+void edit_vector_in_case_of_error(std::vector<float>&grades,std::vector<int>&weights_of_grades,int pos,int err_code){
+	int ARG;
+	if(err_code == GRADES_ERROR){
+		std::cout<<"Grade is grater then 6"<<std::endl;
+		std::cin>>ARG;
+		grades[pos] = ARG;
+	}
+	if(err_code == WEIGHTS_OF_GRADES_ERROR){
+		std::cout<<"weights of this grade is grater then 6"<<std::endl;
+		std::cin>>ARG;
+		weights_of_grades[pos] = ARG;
+	}
+}
+
 int main()
 {
 	cout << "---------------------------------------------------------------------\n";
@@ -15,19 +33,39 @@ int main()
 	cin >> number_of_grades; 
 	cout << "\n";
  
-	float grades[8]; // dynamic allocation of memory, pointers?
-	int weights_of_grades[8]; // 
- 
+	std::vector<float>grades; // vector zamiast tablicy ponieważ automatycznie alokuje pamięć i nie prowadzi do buffer overflow
+	float inital_grade; //zmienna uzywana do przechowania liczby by potem dodać ją do vectora grades
+
+	std::vector<int>weights_of_grades;
+	int uinital_grade;
+
 	int nominator = 0;
- 
 	for (nominator; nominator < number_of_grades; nominator++) // ta petla ma za zadanie zapelnienie tablic ocenami i wagami
 	{
 		cout << "\nWpisz ocene: ";
-		cin >> grades[nominator];
- 
+		while(!(std::cin>>inital_grade)){
+			std::cout<<"Podaj wartosc liczbowa"<<std::endl;
+			std::cin.clear(); //usuwa flagi błędu
+        	std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');//ignoruje znaki w buforze wejściowym do '\n'
+		}
+
+		grades.push_back(inital_grade);//Dodanie wartość inital_grade do końca wektora
+
 		cout << "Wpisz wage: ";
-		cin >> weights_of_grades[nominator];
- 
+		while(!(std::cin>>uinital_grade)){
+			std::cout<<"Podaj wartosc liczbowa"<<std::endl;
+			std::cin.clear();
+        	std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+		}
+ 		weights_of_grades.push_back(uinital_grade);
+
+		if(grades[nominator] > 6){
+			edit_vector_in_case_of_error(grades,weights_of_grades,nominator,GRADES_ERROR);
+		}
+		if(weights_of_grades[nominator] > 6){
+			edit_vector_in_case_of_error(grades,weights_of_grades,nominator,WEIGHTS_OF_GRADES_ERROR);
+		}
+
 	}
  
 	float main_nominator = 0;
@@ -58,7 +96,7 @@ int main()
     cout.precision(2);
 	cout << "\nPercentage: " << result; // !!! result bedzie wyswietlany z dokladnoscia co do dwoch liczb po przecinku
 	cout << "\n\nYour percent gives grade";
- 
+
 	if (result > 0 && result < 30)
 		cout << " insufficient";
 	if (result >= 30 && result < 45)
